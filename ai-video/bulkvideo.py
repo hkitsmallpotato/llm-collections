02df99c640eb17279f32205d48aab09ac07b959b
Lemontea <lemontea.Tom@gmail.com>
2023-07-28 06:38:40 +0800

    (WIP) complete the bulkvideo script

--- ai-video/bulkvideo.py
+++ ai-video/bulkvideo.py
@@ -1,5 +1,11 @@
 import boto3
-import os, json, subprocess, time
+import os, sys, json, subprocess, time
+
+from os import listdir
+from os.path import isfile, join
+
+def get_file_only(mypath):
+    return [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
 # Initialize and setup
 print("Initialize...")
@@ -31,14 +37,27 @@ for idx, req in enumerate(bulk_request):
         print("---- Request {i} of {n}: prompt = {prompt}, file = {file}".format(
             i=idx+1, n=num_req, prompt=req['prompt'], file=req['file']))
         start_time = time.time()
-        local_path = ""
+        prompt = req['prompt']
+        negative_prompt = "text, watermark, copyright, blurry, low resolution, blur, low quality"
+        output_path = "/content/output/" + req['file']
+        #python inference.py -m "/content/model" -p {prompt} -n {negative} 
+        # -W 576 -H 320 -o /content/outputs -d cuda -x -s {num_steps} 
+        # -g {guidance_scale} -f {fps} -T {num_frames}
         result = subprocess.run(
-            [sys.executable, ""], 
-            capture_output=True, text=True
+            [sys.executable, "inference.py", "-m", "/content/model",
+            "-p", prompt, "-n", negative_prompt, "-W", 576, "-H", 320,
+            "-o", output_path,
+            "-d", "cuda", "-x", "-s", 25, "-g", 23, 
+            "-f", 10, "-T", 30], 
+            capture_output=True, text=True,
+            cwd="/content/Text-To-Video-Finetuning"
         )
         print("stdout:\n", result.stdout)
         print("stderr:\n", result.stderr)
         gen_time = time.time()
+        # Need a hack to get the local file
+        local_path = join(output_path, get_file_only(output_path)[0])
+        print("[Debug: {lp}]".format(lp=local_path))
         print("Uploading...")
         bucket.upload_file(local_path, s3_basepath + "/outs/" + req['file'])
         end_time = time.time()
